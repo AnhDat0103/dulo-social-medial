e60937fc387ba818bd5f749ccd33e6788a7dc26e
Nguyễn Trọng Đạt <124783881+AnhDat0103@users.noreply.github.com>
2025-08-28 19:54:50 +0700

    refactor CustomAuthenticationEntryPoint; remove unused imports and clean up code

--- src/main/java/com/dulo/chat_platform/exception/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/dulo/chat_platform/exception/CustomAuthenticationEntryPoint.java
@@ -6,17 +6,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Component
