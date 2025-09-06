084ac8cc08529bc67412f665d21743b0e9125670
Nguyễn Trọng Đạt <124783881+AnhDat0103@users.noreply.github.com>
2025-09-06 17:57:19 +0700

    enhance attachment management; add endpoint to retrieve all attachments for a post

--- src/main/java/com/dulo/chat_platform/controller/AttachmentController.java
+++ src/main/java/com/dulo/chat_platform/controller/AttachmentController.java
@@ -5,10 +5,9 @@ import com.dulo.chat_platform.dto.response.ApiResponse;
 import com.dulo.chat_platform.dto.response.PostAttachmentResponse;
 import com.dulo.chat_platform.service.AttachmentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/attachments")
@@ -25,4 +24,15 @@ public class AttachmentController {
                 .data(attachmentService.createAttachment(postAttachmentRequest))
                 .build();
     }
+
+    @GetMapping()
+    public ApiResponse<List<PostAttachmentResponse>> getAllAttachment(@RequestParam("post-id") int postId){
+        return ApiResponse.<List<PostAttachmentResponse>>builder()
+                .code("200")
+                .message("Get all attachment")
+                .data(attachmentService.getAllAttachment(postId))
+                .build();
+    }
+
+
 }
